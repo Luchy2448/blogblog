229b87f43e1c456413f92353abf122b4afd27534
Luchy2448 <130091006+Luchy2448@users.noreply.github.com>
2024-05-08 19:49:42 -0300

    Creacion de PostObserver

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -26,6 +26,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        \App\Models\Post::observe(\App\Observers\PostObserver::class);
     }
 
     /**
