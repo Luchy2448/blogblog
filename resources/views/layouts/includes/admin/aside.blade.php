c97eabeebc9dba863fcbe5e2648c8108cc5b975c
Luchy2448 <130091006+Luchy2448@users.noreply.github.com>
2024-06-26 22:09:19 -0300

    Ocultación de links por permisos

--- resources/views/layouts/includes/admin/aside.blade.php
+++ resources/views/layouts/includes/admin/aside.blade.php
@@ -4,37 +4,43 @@
               'name' => 'Dashboard',
               'url' => route('admin.dashboard'),
               'active' => request()->routeIs('admin.dashboard'),
-              'icon' => 'fa-solid fa-gauge-high'
+              'icon' => 'fa-solid fa-gauge-high',
+              'can' => ['Acceso al dashboard']
           ],
           [
               'name' => 'Catogorías',
               'url' => route('admin.categories.index'),
               'active' => request()->routeIs('admin.categories.*'),
-              'icon' => 'fa-solid fa-list'
+              'icon' => 'fa-solid fa-list',
+              'can' => ['Gestion de categorías']
           ],
           [
               'name' => 'Artículos',
               'url' => route('admin.posts.index'),
               'active' => request()->routeIs('admin.posts.*'),
-              'icon' => 'fa-solid fa-newspaper'
+              'icon' => 'fa-solid fa-newspaper',
+              'can' => ['Gestion de artículos']
           ],
           [
               'name' => 'Roles',
               'url' => route('admin.roles.index'),
               'active' => request()->routeIs('admin.roles.*'),
-              'icon' => 'fa-solid fa-user-tag'
+              'icon' => 'fa-solid fa-user-tag',
+              'can' => ['Gestion de roles']
          ],
             [
               'name' => 'Permisos',
               'url' => route('admin.permissions.index'),
               'active' => request()->routeIs('admin.permissions.*'),
-              'icon' => 'fa-solid fa-key'
+              'icon' => 'fa-solid fa-key',
+              'can' => ['Gestion de permisos']
           ],
             [
               'name' => 'Usuarios',
               'url' => route('admin.users.index'),
               'active' => request()->routeIs('admin.users.*'),
               'icon' => 'fa-solid fa-users',
+              'can' => ['Gestion de usuarios']
           ]
       ]
     
@@ -51,13 +57,15 @@
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
        <ul class="space-y-2 font-medium">
           @foreach ($links as $link)
-            <li>
+           @canany($link['can'] ?? [null]) 
+           <li>           
                <a href="{{ $link['url'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
                 
                   <i class="{{ $link['icon'] }} w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
                   <span class="ml-3">{{ $link['name'] }}</span>
                </a>
             </li>
+            @endcanany
           @endforeach
        </ul>
     </div>
